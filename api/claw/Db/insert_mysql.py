5e73969e48052a1f950371a573c54d9c548ea07b
Toanf2103 <nobitakute002@gmail.com>
2023-09-23 13:25:09 +0700

    update add db

--- api/claw/Db/insert_mysql.py
+++ api/claw/Db/insert_mysql.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import json
 import os
+import re
+from unidecode import unidecode
 
 current_directory = os.path.dirname(__file__)
 print(current_directory)
@@ -19,8 +21,8 @@ class connect():
         )
         self.mycursor = self.mydb.cursor()
 
-    def clean_database(self):
-        sql= "DELETE FROM artists"
+    def clean_database(self,table):
+        sql= f"DELETE FROM {table}"
         self.mycursor.execute(sql)
         self.mydb.commit()
     def close(self):
@@ -31,14 +33,16 @@ def add_song_to_db(list_song):
     mycursor = conn.mycursor
     mydb = conn.mydb
 
-    sql_artist = 'INSERT IGNORE INTO artists(id,name,link,thumbnail) VALUES '
-    sql_song= 'INSERT IGNORE INTO songs(id,name,duration,source,thumbnail,id_artist) VALUES '
+    sql_artist = 'INSERT IGNORE INTO users(id,name,username,password,is_celeb,slug,avatar) VALUES '
+    sql_song= 'INSERT IGNORE INTO songs(id,id_user,duration,source,thumbnail,slug) VALUES '
 
     for song in list_song:
         artist = song['artist']
+        slug_song = create_slug(song['name']) + "_" + create_slug(artist['name'])
+        slug_artist = create_slug(artist['name'])
         song["name"] = song["name"].replace("\"","'")
-        sql_artist += f'("{artist["id"]}", "{artist["name"]}", "{artist["link"]}","{artist["thumbnail"]}"),'
-        sql_song += f'("{song["id"]}", "{song["name"]}", "{song["duration"]}", "{song["source"]}", "{song["thumbnail"]}", "{artist["id"]}"),'
+        sql_artist += f'("{artist["id"]}", "{artist["name"]}", "{artist["id"]}", "1", 1, "{slug_artist}" , "{artist["thumbnail"]}"),'
+        sql_song += f'( "{song["id"]}", "{artist["id"]}", "{song["duration"]}", "{song["source"]}", "{song["thumbnail"]}", "{slug_song}" ),'
 
     sql_artist=sql_artist.rstrip(",")
     sql_song=sql_song.rstrip(",")
@@ -53,6 +57,31 @@ def add_song_to_db(list_song):
 
     return True
 
+def create_slug(name, separator='-'):
+    name = unidecode(name)
+    # Convert to lowercase
+    name = name.lower()
+
+    # Replace spaces with the specified separator
+    name = name.replace(' ', separator)
+
+    # Remove non-alphanumeric characters (except for the separator)
+    name = re.sub(r'[^a-z0-9' + re.escape(separator) + ']', '', name)
+
+    # Remove leading and trailing separators
+    name = name.strip(separator)
+
+    # Remove consecutive separators
+    name = re.sub(r'[' + re.escape(separator) + ']+', separator, name)
+
+    return name
+
+
+
+
+
+
+
 
 
 def get_list_song_from_json(name_json):
@@ -66,13 +95,13 @@ def get_list_song_from_json(name_json):
 
 
 data = get_list_song_from_json(JSON_SONGS_PATH)
-print(len(data))
-len_data = len(data)
-if(len_data>0):
-    for i in range(0,len_data,500):
-        add_song_to_db(data[i:i+500])
-        print(f'{i} xong')
+
+# if(len_data>0):
+#     for i in range(0,len_data,500):
+#         add_song_to_db(data[i:i+500])
+#         print(f'{i} xong')
 
 # conn = connect()
-# conn.clean_database()
+# conn.clean_database("songs")
 # conn.close()
+
